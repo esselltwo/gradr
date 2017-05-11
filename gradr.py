3cb8ea51a98d40e87c80e9a9a145fb9d47574c86
esselltwo <cmcs@math.berkeley.edu>
2017-05-11 14:08:28 -0700

    Started work on object-oriented version

--- gradr.py
+++ gradr.py
@@ -2,14 +2,10 @@
 
 import csv
 
-#Data is kept in a dictionary
-#keys are student names and the values are lists of scores
-
 #Constants
 #Names and numerical values of the grades that can be assigned
 GradeNames = ['F', 'D-', 'D', 'D+', 'C-', 'C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+']
 GradeNumbers = [0,2,3,4,5,6,7,8,9,10,11,12,13]
-
 
 def parseScores(scores):
     #Converts list of strings to floats, treating empty strings as 0.0
@@ -20,6 +16,68 @@ def parseScores(scores):
         else:
             output.append(float(a))
     return output
+
+class Gradebook:
+    #Tracks grading rules, students, and scores
+
+    def __init__(self):
+        #gradeCategories should be a list of category names
+        #ex ['Homework', 'Quiz', 'Total Section']
+        #or ['Section', 'Midterm', 'Final', 'Total']
+        #these will be used as dict keys, so they need to be unique
+        self.gradeCategories = []
+
+        #table is a dict with keys student IDs and values dicts of scores
+        #the lists of grades should correspond to categories
+        self.table = {}
+
+        #names is a dict with keys student IDs and values student names
+        self.names = {}
+
+    def importNames(self, filename):
+        #Takes a CSV file with names and student IDs and initializes
+        #the table and name dicts with appropriate data
+
+        with open(filename) as file:
+        #filename should be a CSV with the first column names and the second IDs
+        #other columns will be ignored
+            nameReader = csv.reader(file)
+            for row in nameReader:
+                table[row[1]] = {}
+                names[row[1]] = row[0]
+
+    def importScaledScore(self, filename):
+        #This method computes scaled scores (including drops)
+        #for each student and adds them to the gradebook
+        #Intended for things like dropping lowest N quizzes and averaging remainder
+
+        #filename is a CSV file with individual assignment scores
+        #first column is student ids (with first row blank)
+        #second column is number of assignments to drop (with first row blank)
+        #remaining columns are assignments (with first row max points)
+
+        with open(filename) as file:
+            scoreReader = csv.reader(file)
+
+            firstRow = True
+            for row in scoreReader :
+                if firstRow: #Extract the first row to get the max possible points
+                    category = row[1] #second column has category name
+                    maxScores = parseScores(row[2:]) #first two columns are blank
+                    firstRow = False
+                else:
+                    scores = parseScores(row[2:])
+                    scaledScores = []
+                    for maxScore, score in zip(maxScores,scores):
+                        scaledScores.append(score/maxScore)
+                    scaledScores.sort()
+                    toDrop = int(row[1])
+                    scaledScores = scaledScores[toDrop:] #drop lowest toDrop scores
+                    average = sum(scaledScores)/float(len(scaledScores))
+                    self.gradeCategories.append(category)
+                    self.table[row[0]][category] = average
+
+
 
 def initGradeDict(filename):
     #Initializes the dictionary of grades with names
